50a5883934a75de01ad4eacf741c8349f84bd75b
gabriellamiesner <ella.miesner@gmail.com>
2022-10-18 10:28:37 -0500

    adding flask proj

---
+++ flask_proj/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from t import create_report
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/report-submit/')
+def report_submit():
+  file = request.args.get("filename")
+  release_name = request.args.get("news_release_title")
+  release_date = request.args.get("news_release_date")
+  pdf_title = request.args.get("report_title")
+  release_url = request.args.get("url")
+  # recipient_email = request.args.get("recipient_email")
+  create_report(file, release_date, release_name, release_url, pdf_title)
+  print(file, release_name, release_date, pdf_title, release_url)
+  return 'Report has been generated.'
+
+if __name__ == '__main__':
+  app.run(debug=True)
---
+++ flask_proj/t.py
@@ -0,0 +1,154 @@
+from re import L
+import re
+from fpdf import FPDF
+import pandas as pd
+import yagmail
+from pyparsing import And
+pdf = FPDF()
+millnames = ['Million','Billion','Trillion']
+
+def human_readable(n):
+    n = int(n)
+    if n >= 1000000 and n < 1000000000:
+        n = f'{round(n / 1000000, 1)} {millnames[0]}'
+    elif n >= 1000000000 and n < 1000000000000:
+        n = f'{round(n / 1000000000, 1)} {millnames[1]}'
+    elif n >= 1000000000000:
+        n = f'{round(n / 1000000000000, 1)} {millnames[2]}'
+    return n
+    
+
+def create_report(file, date, title, url, pdf_title):
+
+    # function for displaying american placements in PDF report
+    def american_placements():
+        if "https://" in i["url"]:
+            blue()
+            times_underline()
+            pdf.cell(0, 10, txt = f'{str(i["date"])} {i["source"]} ({i["reach"]} potential reach)', link = f'{i["url"]}')
+            pdf.cell(0, 10, '', 0, 1)
+            black()
+            times_reg()
+        else:
+            pdf.cell(0, 10, f'{str(i["date"])} {i["source"]} ({i["reach"]} potential reach)', 0, 1 )
+
+    # load csv file, alter data to make processing + displaying easier 
+    data = pd.read_csv(file)
+    data.loc[:, "Reach"] = data["Reach"].round(-3).map('{:,d}'.format)
+    data["State"] = data["State"].fillna(-1)
+
+    # creating spaces for placement seperation, var to hold total potential reach 
+    state_placements = []
+    national_placements = []
+    international_placements = []
+    reach = 0
+    social_echo = 0
+
+    # loop thru entire data set, seperate into international, national, state placements 
+    # add each publications info (date, source, url, country, reach) to seperate buckets for later display
+    # add each publications reach to the running total
+    count = 0
+    for i in data['State']: 
+        if data['Country'][count] != "United States":
+            international_placements.append({"date": data['Alternate Date Format'][count], "source": data["Source"][count],  "url": data["URL"][count], "country": data["Country"][count], "reach": data["Reach"][count]})
+            reach += int(data["Reach"][count].replace(",", ""))
+            social_echo += int(data["Twitter Social Echo"][count]) + int(data["Facebook Social Echo"][count]) + int(data["Reddit Social Echo"][count])
+        if i != "Missouri" and i != -1:
+            national_placements.append({"date": data['Alternate Date Format'][count], "source": data["Source"][count], "url": data["URL"][count], "state": data["State"][count], "reach": data["Reach"][count]})
+            reach += int(data["Reach"][count].replace(",", ""))
+            social_echo += int(data["Twitter Social Echo"][count]) + int(data["Facebook Social Echo"][count]) + int(data["Reddit Social Echo"][count])
+        if i == "Missouri": 
+            state_placements.append({"date": data['Alternate Date Format'][count], "source": data["Source"][count], "url": data["URL"][count], "city": data["City"][count], "reach": data["Reach"][count]})
+            reach += int(data["Reach"][count].replace(",", ""))
+            social_echo += int(data["Twitter Social Echo"][count]) + int(data["Facebook Social Echo"][count]) + int(data["Reddit Social Echo"][count])
+
+
+        count += 1
+
+        
+    # creation of PDF report 
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    times_reg()
+    pdf.cell(0, 10, f'{date}', 0, 1)
+    times_bold()
+    pdf.cell(0, 10, 'News Release: ', 0, 10)
+    times_underline()
+    blue()
+    pdf.cell(0, txt = f'{title}', link = f'{url}')
+    spacer()
+    times_bold()
+    black()
+    pdf.cell(0, 10, "State Placements", 0, 1)
+    times_reg()
+
+    for i in state_placements:
+        american_placements()
+
+    # times_bold()
+    # pdf.cell(0, 10, "State Radio Placements", 0, 1)
+    # times_reg()
+    # pdf.cell(0, 10, '8-3-22 Branson Tri-Lakes News (49,000 potential reach, Hollister, MO) ', 0, 1)
+    times_bold()
+    pdf.cell(0, 10, "National Placements", 0, 1)
+    times_reg()
+
+    for i in national_placements:
+        american_placements()
+
+    times_bold()
+    pdf.cell(0, 10, "International Placements", 0, 1)
+    times_reg()
+
+    for i in international_placements:
+        if "https://" in i["url"]:
+            pdf.set_text_color(r = 0, g = 0, b = 255)
+            pdf.set_font('Times', 'U', 12)
+            pdf.cell(0, 10, txt = f'{str(i["date"])} {i["source"]} ({i["reach"]} potential reach, {i["country"]})', link = f'{i["url"]}')
+            pdf.cell(0, 10, '', 0, 1)
+            pdf.set_text_color(r = 0, g = 0, b = 0 )
+            pdf.set_font('Times', '', 12)
+        else:
+            pdf.cell(0, 10, f'{str(i["date"])} {i["source"]} ({i["reach"]} potential reach, {i["country"]})', 0, 1 )
+
+    # calculate total placements, add space, display totals, output PDF report 
+    placements = human_readable(len(state_placements) + len(national_placements) + len(international_placements))
+    reach = human_readable(reach)
+    social_echo = human_readable(social_echo)
+    spacer()
+    pdf.cell(0, 10, f'Totals: {placements} placements, {reach} potential reach, {social_echo} social media impressions')
+    pdf.output(f'{pdf_title}.pdf', 'F')
+
+    # fname = f'{pdf_title}.pdf'
+    # esender = "munewsreports@gmail.com"
+    # ereciever = f'{recipient_email}'
+    # esubject = f'Your Report: {title} is ready'
+    # ebody = '''
+    #     <p>Please find attached the PDF report file as requested.</p>
+    #     <b> MU News Bureau </b>
+    #    '''
+
+    # yag = yagmail.SMTP(esender, 'password')
+    # yag.send(to=ereciever, subject=esubject, contents=ebody, attachments=fname)
+    # print('report was sent')
+
+
+
+# functions to make styling document simpler
+def times_reg():
+    pdf.set_font('Times', '', 12)
+
+def times_bold():
+    pdf.set_font('Times', 'B', 12)
+
+def times_underline():
+    pdf.set_font('Times', 'U', 12)
+
+def blue():
+    pdf.set_text_color(r = 0, g = 0, b = 255 )
+
+def black():
+    pdf.set_text_color(r = 0, g = 0, b = 0 )
+
+def spacer():
+    pdf.cell(0,10, '', 0, 1)
